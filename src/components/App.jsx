093b9b6e17b4d045cba91c01e4d99f4196a769b9
direct66612 <direct66612@gmail.com>
2023-11-13 16:08:03 -0500

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,3 @@
-import user from '../components/user.json';
-import data from '../components/data.json';
-import friends from '../components/friends.json';
-import transactions from '../components/transactions.json';
-import Profile from './Profile/Profile';
-import Statistics from './Statistics/Statistics';
-import FriendList from './FriendList/FriendList';
-import TransactionHistory from './TransactionHistory/TransactionHistory';
 export const App = () => {
-  return (
-    <div>
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-      <Statistics stats={data} />
-      <FriendList friends={friends} />
-      <TransactionHistory item={transactions} />
-    </div>
-  );
+  return <div></div>;
 };
--- src/components/FriendList/FriendItem.jsx
+++
@@ -1,9 +0,0 @@
-export default function FriendItem({ avatar, name, isOnline }) {
-  return (
-    <li className="item">
-      <span className={isOnline ? 'green' : 'red'}></span>
-      <img className="avatar" src={avatar} alt="User avatar" width="48" />
-      <p className="name">{name}</p>
-    </li>
-  );
-}
